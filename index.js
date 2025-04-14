1140fc2c6688d2a64aac07d745bc26dedfe2f222
Vladimir Stoikov <vspsmoke@gmail.com>
2025-04-14 22:40:46 +0300

    add spline.js, create line from spline points

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('all work');
+import * as THREE from 'three';
+import spline from './spline.js';
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 3;
+
+const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.005;
+const detail = 24;
+
+// line geo
+
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({
+  color: 0x00ff00,
+});
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
